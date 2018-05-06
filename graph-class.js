ff91300dfc699d0b2ec508da1b5b34a9c8d8341f
Sean <sjgallen@gmail.com>
2018-05-06 14:21:03 -0500

    more tests and an undirected node functionality

--- graph-class.js
+++ graph-class.js
@@ -1,13 +1,14 @@
 class Graph {
-    constructor(){
-     this.nodes = [];
-    }
-
-    runAMethod() {
-
-    }
-
-    doh() {
-
-    }
+  constructor(){
+    this.nodes = [];
+    this.edges = [];
+  }
+  addNodes(node){
+    this.nodes.push(node);
+    this.edges[node] = [];
+  }
+  addEdges(node1, node2) {
+    this.edges[node1].push(node2);
+    this.edges[node2].push(node1);
+  }
 }
--- graph-class.spec.js
+++ graph-class.spec.js
@@ -6,3 +6,27 @@ describe('Graph class', () => {
         expect(expected).toBe(result);
     })
 })
+
+describe('Graph has edges', () => {
+    it('A Graph with nodes should have edges', () => {
+        const graph = new Graph();
+        var result = Array.isArray(graph.edges);
+        var expected = true;
+        expect(expected).toBe(result);
+    })
+})
+
+describe('Add Nodes and Edges to Graphs', () => {
+    it('Graphs can add Nodes and make connection with Edges', () => {
+        const graph = new Graph();
+        graph.addNodes(1);
+        graph.addNodes(2);
+        graph.addEdges(1, 2);
+        var nodeCount = graph.nodes.length;
+        var edgeCount = graph.edges.length-1; //Minus one because zero index is empty but still counts.
+        var expected = 2;
+        expect(expected).toBe(nodeCount);
+        expect(expected).toBe(edgeCount);
+    })
+})
+
